1d9d2bfda95a11b6cc300f89368eba8364fd66c1
zeyucui1 <zeyu.cui@student.uts.edu.au>
2024-06-09 23:24:22 +1000

    feat: add animation in navbar and work section using Frame Motion libary

--- src/components/Cards/ProjectCards.jsx
+++ src/components/Cards/ProjectCards.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
-const Button = styled.button`
-  display: none;
-  width: 100%;
-  padding: 10px;
-  background-color: ${({ theme }) => theme.white};
-  color: ${({ theme }) => theme.text_black};
-  font-size: 14px;
-  font-weight: 700;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  transition: all 0.8s ease-in-out;
-`
-const Card = styled.div`
+const Card = styled(motion.div)`
   width: 330px;
   height: 490px;
   background-color: ${({ theme }) => theme.card};
@@ -27,17 +15,9 @@ const Card = styled.div`
   flex-direction: column;
   gap: 14px;
   transition: all 0.5s ease-in-out;
-  &:hover {
-    transform: translateY(-10px);
-    box-shadow: 0 0 50px 4px rgba(0, 0, 0, 0.6);
-    filter: brightness(1.1);
-  }
-  &:hover ${Button} {
-    display: block;
-  }
 `
 
-const Image = styled.img`
+const Image = styled(motion.img)`
   width: 100%;
   height: 180px;
   background-color: ${({ theme }) => theme.white};
@@ -46,7 +26,7 @@ const Image = styled.img`
   object-fit: cover;
 `
 
-const Tags = styled.div`
+const Tags = styled(motion.div)`
   width: 100%;
   display: flex;
   align-items: center;
@@ -55,7 +35,7 @@ const Tags = styled.div`
   margin-top: 4px;
 `
 
-const Tag = styled.span`
+const Tag = styled(motion.span)`
   font-size: 12px;
   font-weight: 400;
   color: ${({ theme }) => theme.primary};
@@ -64,14 +44,15 @@ const Tag = styled.span`
   border-radius: 10px;
 `
 
-const Details = styled.div`
+const Details = styled(motion.div)`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 0px;
   padding: 0px 2px;
 `
-const Title = styled.div`
+
+const Title = styled(motion.div)`
   font-size: 20px;
   font-weight: 600;
   color: ${({ theme }) => theme.text_secondary};
@@ -84,7 +65,7 @@ const Title = styled.div`
   text-overflow: ellipsis;
 `
 
-const Date = styled.div`
+const Date = styled(motion.div)`
   font-size: 12px;
   margin-left: 2px;
   font-weight: 400;
@@ -94,7 +75,7 @@ const Date = styled.div`
   }
 `
 
-const Description = styled.div`
+const Description = styled(motion.div)`
   font-weight: 400;
   color: ${({ theme }) => theme.text_secondary + 99};
   overflow: hidden;
@@ -106,12 +87,13 @@ const Description = styled.div`
   text-overflow: ellipsis;
 `
 
-const Members = styled.div`
+const Members = styled(motion.div)`
   display: flex;
   align-items: center;
   padding-left: 10px;
 `
-const Avatar = styled.img`
+
+const Avatar = styled(motion.img)`
   width: 38px;
   height: 38px;
   border-radius: 50%;
@@ -121,23 +103,45 @@ const Avatar = styled.img`
   border: 3px solid ${({ theme }) => theme.card};
 `
 
+const cardVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+  hover: { scale: 1.05, boxShadow: '0 0 50px 4px rgba(0, 0, 0, 0.6)' },
+}
+
+const childVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+}
+
 const ProjectCards = ({ project, setOpenModal }) => {
   return (
-    <Card onClick={() => setOpenModal({ state: true, project: project })}>
-      <Image src={project.image} />
-      <Tags>
+    <Card
+      variants={cardVariants}
+      initial="hidden"
+      animate="visible"
+      whileHover="hover"
+      transition={{ duration: 0.3 }}
+      onClick={() => setOpenModal({ state: true, project: project })}
+    >
+      <Image src={project.image} variants={childVariants} />
+      <Tags variants={childVariants}>
         {project.tags?.map((tag, index) => (
-          <Tag>{tag}</Tag>
+          <Tag key={index} variants={childVariants}>
+            {tag}
+          </Tag>
         ))}
       </Tags>
-      <Details>
-        <Title>{project.title}</Title>
-        <Date>{project.date}</Date>
-        <Description>{project.description}</Description>
+      <Details variants={childVariants}>
+        <Title variants={childVariants}>{project.title}</Title>
+        <Date variants={childVariants}>{project.date}</Date>
+        <Description variants={childVariants}>
+          {project.description}
+        </Description>
       </Details>
-      <Members>
-        {project.member?.map((member) => (
-          <Avatar src={member.img} />
+      <Members variants={childVariants}>
+        {project.member?.map((member, index) => (
+          <Avatar key={index} src={member.img} variants={childVariants} />
         ))}
       </Members>
     </Card>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Nav,
   NavLink,
@@ -18,32 +18,80 @@ import { FaBars } from 'react-icons/fa'
 import { Bio } from '../../data/constants'
 import { HiOutlineMoon, HiOutlineSun } from 'react-icons/hi2'
 import { useTheme } from 'styled-components'
-import { useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   const handleDarkModeChange = () => {
     setDarkMode((isDarkMode) => !isDarkMode)
   }
+
   const [isOpen, setIsOpen] = useState(false)
   const theme = useTheme()
+
+  const hoverAnimation = {
+    scale: 1.1,
+    rotate: 5,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+    },
+  }
+
+  const logoAnimation = useAnimation()
+  const spanAnimation = useAnimation()
+
+  // Start the animation on component mount
+  React.useEffect(() => {
+    logoAnimation.start({
+      rotate: [0, 10, -10, 0],
+      transition: {
+        duration: 2,
+        ease: 'easeInOut',
+        repeat: Infinity,
+      },
+    })
+
+    spanAnimation.start({
+      scale: [1, 1.1, 1],
+      transition: {
+        duration: 1,
+        ease: 'easeInOut',
+        repeat: Infinity,
+      },
+    })
+  }, [logoAnimation, spanAnimation])
+
   return (
     <Nav>
-      <NavbarContainer>
+      <NavbarContainer
+        as={motion.div}
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ type: 'spring', stiffness: 120 }}
+      >
         <NavLogo to="/">
-          <div
+          <motion.div
             style={{
               display: 'flex',
               alignItems: 'center',
               color: 'white',
-              marginBottom: '20;',
+              marginBottom: '20',
               cursor: 'pointer',
             }}
             onClick={() => {
               window.scrollTo(0, 0)
             }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
           >
-            <DiCssdeck size="3rem" /> <Span>Zeyu's Portfolio</Span>
-          </div>
+            <motion.div animate={logoAnimation}>
+              <DiCssdeck size="3rem" />
+            </motion.div>
+            <motion.div animate={spanAnimation}>
+              <Span>Zeyu's Portfolio</Span>
+            </motion.div>
+          </motion.div>
         </NavLogo>
         <MobileIcon>
           <FaBars
@@ -52,14 +100,30 @@ const Navbar = ({ darkMode, setDarkMode }) => {
             }}
           />
         </MobileIcon>
-        <NavItems>
-          <NavLink href="#about">About</NavLink>
-          <NavLink href="#skills">Skills</NavLink>
-          <NavLink href="#experience">Experience</NavLink>
-          <NavLink href="#projects">Projects</NavLink>
-          <NavLink href="#education">Education</NavLink>
+        <NavItems
+          as={motion.div}
+          initial={{ x: -200 }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 120 }}
+        >
+          {['About', 'Skills', 'Experience', 'Projects', 'Education'].map(
+            (item) => (
+              <motion.div
+                key={item}
+                whileHover={hoverAnimation}
+                style={{ display: 'inline-block' }}
+              >
+                <NavLink href={`#${item.toLowerCase()}`}>{item}</NavLink>
+              </motion.div>
+            )
+          )}
         </NavItems>
-        <ButtonContainer>
+        <ButtonContainer
+          as={motion.div}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <GitHubButton href={Bio.github} target="_blank">
             Github Profile
           </GitHubButton>
@@ -68,47 +132,31 @@ const Navbar = ({ darkMode, setDarkMode }) => {
           {darkMode ? <HiOutlineMoon /> : <HiOutlineSun />}
         </DarkModeButton>
         {isOpen && (
-          <MobileMenu isOpen={isOpen}>
-            <MobileLink
-              href="#about"
-              onClick={() => {
-                setIsOpen(!isOpen)
-              }}
-            >
-              About
-            </MobileLink>
-            <MobileLink
-              href="#skills"
-              onClick={() => {
-                setIsOpen(!isOpen)
-              }}
-            >
-              Skills
-            </MobileLink>
-            <MobileLink
-              href="#experience"
-              onClick={() => {
-                setIsOpen(!isOpen)
-              }}
-            >
-              Experience
-            </MobileLink>
-            <MobileLink
-              href="#projects"
-              onClick={() => {
-                setIsOpen(!isOpen)
-              }}
-            >
-              Projects
-            </MobileLink>
-            <MobileLink
-              href="#education"
-              onClick={() => {
-                setIsOpen(!isOpen)
-              }}
-            >
-              Education
-            </MobileLink>
+          <MobileMenu
+            isOpen={isOpen}
+            as={motion.div}
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ type: 'spring', stiffness: 300 }}
+          >
+            {['About', 'Skills', 'Experience', 'Projects', 'Education'].map(
+              (item) => (
+                <motion.div
+                  key={item}
+                  whileHover={hoverAnimation}
+                  style={{ display: 'block' }}
+                >
+                  <MobileLink
+                    href={`#${item.toLowerCase()}`}
+                    onClick={() => {
+                      setIsOpen(!isOpen)
+                    }}
+                  >
+                    {item}
+                  </MobileLink>
+                </motion.div>
+              )
+            )}
             <GitHubButton
               style={{
                 padding: '10px 16px',
